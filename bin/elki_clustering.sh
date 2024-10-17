fabb1b1f8248524e62627563ef1b02b93232b1aa
Vladimir Mikryukov <v.mikryukov@gmail.com>
2024-10-17 17:47:42 +0300

    initialize `elki_clustering` script

---
+++ bin/elki_clustering.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+## Usage:
+# ./elki_clustering.sh \
+#   --input input.csv \
+#   --output output.csv \
+#   --method OPTICS \
+#   --geomodel WGS84SpheroidEarthModel \
+#   --epsilon 100000 \
+#   --minpts 5 \
+#   --indextype RStarTree
+
+## Path to ELKI
+ELKI="~/bin/elki-bundle-0.8.0.jar"
+
+## Parameters
+INPUT=""
+OUTPUT=""
+METHOD=""
+GEOMODEL=""
+EPSILON=""
+MINPTS=""
+INDEXTYPE=""
+
+## Parse command line arguments
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --input)
+            INPUT="$2"
+            shift 2
+            ;;
+        --output)
+            OUTPUT="$2"
+            shift 2
+            ;;
+        --method)
+            METHOD="$2"
+            shift 2
+            ;;
+        --geomodel)
+            GEOMODEL="$2"
+            shift 2
+            ;;
+        --epsilon)
+            EPSILON="$2"
+            shift 2
+            ;;
+        --minpts)
+            MINPTS="$2"
+            shift 2
+            ;;
+        --indextype)
+            INDEXTYPE="$2"
+            shift 2
+            ;;
+        *)
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+    esac
+done
+
+## Validate required arguments
+if [[ -z $INPUT || -z $OUTPUT || -z $METHOD || -z $GEOMODEL || -z $EPSILON || -z $MINPTS || -z $INDEXTYPE ]]; then
+    echo "Usage: $0 --input <input_file> --output <output_file> --method <OPTICS|DBSCAN> --geomodel <geomodel> --epsilon <epsilon> --minpts <minpts> --indextype <MTree|RStarTree>"
+    exit 1
+fi
+
+## Validate method
+if [[ $METHOD != "OPTICS" && $METHOD != "DBSCAN" ]]; then
+    echo "Error: Method must be either OPTICS or DBSCAN"
+    exit 1
+fi
+
+## Validate nearest neighbor index type
+if [[ $INDEXTYPE != "MTree" && $INDEXTYPE != "RStarTree" ]]; then
+    echo "Error: Indextype must be either MTree or RStarTree"
+    exit 1
+fi
+
+## Run ELKI clustering
+java -jar "$ELKI" ..
+
+echo -e "\nClustering completed\n"
+
