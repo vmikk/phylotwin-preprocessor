c7f4f2d1fecd39fad4cbd007781edcf112018ab4
Vladimir Mikryukov <v.mikryukov@gmail.com>
2024-10-23 13:03:58 +0300

    add outlier visualization script

---
+++ bin/visualize_outlier_score.R
@@ -0,0 +1,130 @@
+#!/usr/bin/env Rscript
+
+## Function to load packages
+load_pckg <- function(pkg = "data.table"){
+    suppressPackageStartupMessages( library(package = pkg, character.only = TRUE) )
+    cat(paste(pkg, packageVersion(pkg), "\n"))
+}
+
+load_pckg("data.table")
+load_pckg("plyr")
+load_pckg("sf")
+load_pckg("rnaturalearth")
+load_pckg("ggplot2")
+load_pckg("patchwork")
+load_pckg("optparse")
+
+theme_set(theme_bw())
+
+
+## Define the option parser
+option_list <- list(
+    make_option(c("-i", "--input"),
+        type = "character", default = NULL,
+        help = "Input coordinates (CSV format)"),
+    make_option(c("-o", "--outlier"),
+        type = "character", default = NULL,
+        help = "Outlier scores (TSV format)"),
+    make_option(c("-p", "--plot"),
+        type = "character", default = NULL,
+        help = "Output plots (PNG format)")
+)
+
+## Parse the command line arguments
+opt <- parse_args(OptionParser(option_list = option_list))
+
+## Input parameters
+INPUT <- opt$input
+OUTLIER <- opt$outlier
+PLOT <- opt$plot
+
+## Load world map
+cat("..Loading world map\n")
+world <- ne_countries(scale = "medium", returnclass = "sf")
+
+## Load coordinates
+cat("..Loading coordinates\n")
+CRD <- fread(file = INPUT, header = F, col.names = c("Latitude", "Longitude", "ID"))
+
+## Load outlier scores
+cat("..Loading outlier scores\n")
+SCR <- fread(file = OUTLIER, header = F, col.names = c("ID", "OutlierScore"))
+
+## Combine coordinates and outlier scores
+cat("..Combining coordinates and outlier scores\n")
+CRD$OutlierScore <- SCR$OutlierScore
+
+## Plotting function
+## with optional outlier bubbles (circle around point proportional to the score)
+show_occ <- function(x, outlier_mode = NULL, threshold_multiplier = 5){
+  # x <- copy(CRD)
+  # outlier_mode <- c("low", "medium", "high")  # quantile-based (ArcGIS-style) 
+  # threshold_multiplier <- 5  # since scores can be too hight to display, restrict max values
+
+  ## Quantile-based thresholds for outlier score (as in ArcGIS)
+  if(!is.null(outlier_mode)){
+    q3  <- quantile(x = x$OutlierScore, probs = 0.75, na.rm = TRUE)
+    iqr <- IQR(x$OutlierScore, na.rm = TRUE)
+    if(outlier_mode %in% "low")   { iqr <- iqr * 2   }
+    if(outlier_mode %in% "medium"){ iqr <- iqr * 1.5 }
+    thrsh <- q3 + iqr
+    if(any(is.infinite(x$OutlierScore))){
+      mx <- as.numeric(thrsh * 2)
+      x[ is.infinite(OutlierScore), OutlierScore := mx ]
+    }
+    x[ , Outlier := fifelse(OutlierScore < thrsh, 0, 1, na=NA) ]
+  
+    ## Restrict max values of thresholds to show on a map
+    x[ , OutlierScore := OutlierScore ]
+    x[ OutlierScore > thrsh * threshold_multiplier, OutlierScore := thrsh * threshold_multiplier ]
+
+  } else {
+    x[ , Outlier := 0 ]
+  }
+
+  x$Outlier <- factor(x$Outlier, levels = c(0, 1))
+
+  xx <- sf::st_as_sf(x,
+    coords = c("Longitude", "Latitude"),
+    crs = 4326)
+
+  pp <- ggplot() + 
+    geom_sf(data = world)
+
+  ## Show outlier bubbles
+  if( !is.null(outlier_mode) ){
+
+    pp <- pp +
+      geom_sf(data = xx, aes(size = OutlierScore, color = Outlier), shape = 21) + 
+      scale_color_manual(values = c("blue", "red")) +
+      geom_sf(data = xx, aes(color = Outlier), size = 1) +
+      labs(size = "Outlier score")
+  
+  } else {
+
+    pp <- pp + 
+      geom_sf(data = xx, color = "blue")
+
+  }
+
+  return(pp)
+}
+
+## Plot
+cat("..Plotting\n")
+p1 <- show_occ(CRD, outlier_mode = "low")
+p2 <- show_occ(CRD, outlier_mode = "medium")
+p3 <- show_occ(CRD, outlier_mode = "high")
+
+## Arrange plots in a row
+cat("..Arranging plots\n")
+pp <- p1 + p2 + p3 + plot_layout(ncol = 1)
+
+## Export plots
+cat("..Exporting plots\n")
+ggsave(
+  filename = PLOT,
+  plot = pp,
+  width = 15, height = 22,
+  units = "in", dpi = 300)
+
