2c16fac76380f7297c679aa396716bf9498e7b98
Vladimir Mikryukov <v.mikryukov@gmail.com>
2024-11-29 15:37:32 +0200

    `pool_occurrence_counts` - add extinct taxa arg

--- bin/pool_occurrence_counts.R
+++ bin/pool_occurrence_counts.R
@@ -31,7 +31,10 @@ option_list <- list(
         help = "Output file with a list of species with large number of occurrences"),
     make_option(c("-s", "--output_small"),
         type = "character", default = NULL,
-        help = "Output file with a list of species with small number of occurrences")
+        help = "Output file with a list of species with small number of occurrences"),
+    make_option(c("-e", "--extinct"),
+        type = "character", default = NULL,
+        help = "Extinct species list (optional); text file with specieskeys in the first column, with header")
 )
 
 ## Parse the command line arguments
@@ -41,12 +44,13 @@ opt <- parse_args(OptionParser(option_list = option_list))
 INPDIR <- opt$inpdir
 OUTPUT_LARGE <- opt$output_large
 OUTPUT_SMALL <- opt$output_small
+EXTINCT <- opt$extinct
 
 cat("\nInput parameters:\n")
 cat("..Input directory:", INPDIR, "\n")
 cat("..Output [large]:", OUTPUT_LARGE, "\n")
 cat("..Output [small]:", OUTPUT_SMALL, "\n")
-
+cat("..Extinct taxa:", EXTINCT, "\n")
 ## List files in the input directory
 fls_large <- list.files(path = INPDIR, pattern = "*_large.txt", full.names = TRUE)
 fls_small <- list.files(path = INPDIR, pattern = "*_small.txt", full.names = TRUE)
@@ -60,7 +64,7 @@ cat(paste(fls_small, collapse = "\n"))
 cat("\n")
 
 ## Load data
-cat("\nLoading data:\n")
+cat("\nLoading data species occurrences:\n")
 
 load_fls <- function(x){
   # x = file name
@@ -75,6 +79,12 @@ occ_small <- alply(.data = fls_small, .margins = 1, .fun = load_fls)
 occ_large <- rbindlist(occ_large)
 occ_small <- rbindlist(occ_small)
 
+if(!is.null(EXTINCT)){
+  cat("\nLoading extinct taxa:\n")
+  extinct <- fread(EXTINCT)
+  cat("..Number of records in extinct taxa:", nrow(extinct), "\n")
+}
+
 cat("\nKeeping only unique species\n")
 
 occ_large <- unique(occ_large, by = "specieskey")
@@ -82,6 +92,15 @@ occ_small <- unique(occ_small, by = "specieskey")
 
 cat("Number of unique specieskeys [large]:", nrow(occ_large), "\n")
 cat("Number of unique specieskeys [small]:", nrow(occ_small), "\n")
+
+if(!is.null(EXTINCT)){
+  cat("\nRemoving extinct species\n")
+  occ_large <- occ_large[ !specieskey %in% extinct$specieskey ]
+  occ_small <- occ_small[ !specieskey %in% extinct$specieskey ]
+
+  cat("..Number of unique specieskeys after removing extinct [large]:", nrow(occ_large), "\n")
+  cat("..Number of unique specieskeys after removing extinct [small]:", nrow(occ_small), "\n")
+}
 
 setorder(occ_large, specieskey)
 setorder(occ_small, specieskey)
